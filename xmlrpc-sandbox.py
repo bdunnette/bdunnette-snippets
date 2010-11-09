2edb9f34bdde9d7b56d8fafd4509a55a627721d2
Brian Dunnette <brian@dunnette.us>
2010-11-09 16:12:59 +0000

    playing with XML-RPC, currently just connects to server and lists info about available methods

---
+++ xmlrpc-sandbox.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import xmlrpclib
+
+endpoint = "http://user:pass@server:8069/tryton"
+server = xmlrpclib.ServerProxy(endpoint)
+
+try:
+    server_method_list = server.system.listMethods()
+    print server_method_list
+    server_method_help = ""
+    for method in server_method_list:
+        method_help = server.system.methodHelp(method)
+        if method != "system.multicall":
+            method_signature = server.system.methodSignature(method)
+        else:
+            method_signature = ""
+        print method, method_help, method_signature
+except xmlrpclib.ProtocolError, err:
+    print "A protocol error occurred"
+    print "URL: %s" % err.url
+    print "HTTP/HTTPS headers: %s" % err.headers
+    print "Error code: %d" % err.errcode
+    print "Error message: %s" % err.errmsg
+except xmlrpclib.Fault, err:
+    print "A fault occurred"
+    print "Fault code: %d" % err.faultCode
+    print "Fault string: %s" % err.faultString
+
+
