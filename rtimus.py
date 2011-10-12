c1e651058d092c2fcd973c86c280e491d6a00cde
Brian Dunnette <dunn0172@umn.edu>
2011-10-12 11:40:06 -0500

    Changes made by SparkleShare

--- rtimus.py
+++ rtimus.py
@@ -13,11 +13,12 @@ if os.access(os.environ['HOME']+'/.xsend',os.R_OK):
     for ln in open(os.environ['HOME']+'/.xsend').readlines():
         key,val=ln.strip().split('=',1)
         jidparams[key.lower()]=val
-	for mandatory in ['jid','password']:
-	    if mandatory not in jidparams.keys():
-		open(os.environ['HOME']+'/.xsend','w').write('#JID=romeo@montague.net\n#PASSWORD=juliet\n')
-		print 'Please ensure the ~/.xsend file has valid JID for sending messages.'
-    		sys.exit(0)
+
+for mandatory in ['jid','password']:
+    if mandatory not in jidparams.keys():
+	open(os.environ['HOME']+'/.xsend','w').write('#JID=romeo@montague.net\n#PASSWORD=juliet\n')
+	print 'Please ensure the ~/.xsend file has valid JID for sending messages.'
+	sys.exit(0)
 
 jid=xmpp.protocol.JID(jidparams['jid'])
 cl=xmpp.Client(jid.getDomain(),debug=[])
