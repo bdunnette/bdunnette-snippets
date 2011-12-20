2a50ed737295f4d248f60516a20e172ffb962373
Brian Dunnette <dunn0172@umn.edu>
2011-12-20 13:30:56 -0600

    / ‘jsonrpc-sandbox.py’

--- jsonrpc-sandbox.py
+++ jsonrpc-sandbox.py
@@ -1,4 +1,9 @@
+from jsonrpclib import Server as ServerProxy
 import jsonrpclib
-server = jsonrpclib.Server('http://admin:test@localhost:8000')
-methods = server.Introspect()
-print methods
+import json
+
+server = ServerProxy('http://admin:test@localhost:8000')
+try:
+    server.common.db.list(None,None)
+    a = json.loads( jsonrpclib.history.response)
+    print(a["result"])
