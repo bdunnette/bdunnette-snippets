fca0c49a895c0d9f877114c4365fc1816fea447c
Brian Dunnette <dunn0172@umn.edu>
2011-12-20 13:31:40 -0600

    / ‘jsonrpc-sandbox.py’

--- jsonrpc-sandbox.py
+++ jsonrpc-sandbox.py
@@ -7,3 +7,7 @@ try:
     server.common.db.list(None,None)
     a = json.loads( jsonrpclib.history.response)
     print(a["result"])
+
+except:
+    a = json.loads( jsonrpclib.history.response)
+    print "error: ", a["error"]
