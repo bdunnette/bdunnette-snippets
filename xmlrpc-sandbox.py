715228252e078984af598ee9394da0790000a498
Brian Dunnette <dunn0172@umn.edu>
2012-01-04 15:06:00 -0600

    / ‘xmlrpc-sandbox.py’

--- xmlrpc-sandbox.py
+++ xmlrpc-sandbox.py
@@ -6,16 +6,7 @@ endpoint = sys.argv[1]
 server = xmlrpclib.ServerProxy(endpoint)
 
 try:
-    server_method_list = server.system.listMethods()
-    print server_method_list
-    server_method_help = ""
-    for method in server_method_list:
-        method_help = server.system.methodHelp(method)
-        if method != "system.multicall":
-            method_signature = server.system.methodSignature(method)
-        else:
-            method_signature = ""
-        print method, method_help, method_signature
+    server.__request('model.party.party.search', params)
 except xmlrpclib.ProtocolError, err:
     print "A protocol error occurred"
     print "URL: %s" % err.url
