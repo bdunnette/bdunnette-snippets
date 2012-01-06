150c02fa6a8f6b82023c67cdf0d7d07abec17fa5
Brian Dunnette <dunn0172@umn.edu>
2012-01-06 12:19:12 -0600

    / ‘ledger-sandbox.py’

--- ledger-sandbox.py
+++ ledger-sandbox.py
@@ -4,4 +4,4 @@ from beancount.ledger import Ledger
 
 ledger_file = open("fgtc-2011.journal")
 ledger = Ledger()
-ledger.parse_file(ledger_file, 'fgtc-2011.journal', encoding)
+ledger.parse_file(ledger_file, 'fgtc-2011.journal')
