b9e50cfcb858fcd79bd12580f4175758e10aa1fd
Brian Dunnette <dunn0172@umn.edu>
2012-01-06 14:43:40 -0600

    / ‘ledger-sandbox.py’

--- ledger-sandbox.py
+++ ledger-sandbox.py
@@ -2,9 +2,8 @@
 
 import sys
 from beancount.ledger import Transaction, Ledger
+from beancount import cmdline
 
 ledger_file_name = sys.argv[1]
 ledger_file_object = open(ledger_file_name)
-ledger = Ledger()
-ledger_parsed = ledger.parse_file(ledger_file_object, ledger_file_name)
-print ledger_parsed
+ledger = cmdline.load_ledger("", ledger_file_name, opts)
