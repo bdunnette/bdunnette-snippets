b65ee05239c8223f402bac19451e4ef197fc8824
Brian Dunnette <dunn0172@umn.edu>
2012-01-06 12:53:42 -0600

    / ‘ledger-sandbox.py’

--- ledger-sandbox.py
+++ ledger-sandbox.py
@@ -7,3 +7,4 @@ ledger_file_name = sys.argv[1]
 ledger_file_object = open(ledger_file_name)
 ledger = Ledger()
 ledger_parsed = ledger.parse_file(ledger_file_object, ledger_file_name, 'utf8')
+print ledger_parsed
