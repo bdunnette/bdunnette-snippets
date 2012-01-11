a922502ae900092fc2acc499fa1188c95de84494
Brian Dunnette <dunn0172@umn.edu>
2012-01-11 16:29:44 -0600

    / ‘ledger-invoice.py’

--- ledger-invoice.py
+++ ledger-invoice.py
@@ -21,8 +21,8 @@ for transaction in transactions:
     desc_line = transaction_lines[0].split(' ', 1)
     transaction_dict['date'] = desc_line[0]
     for sep in description_separators:
-        print sep, desc_line   
         if desc_line[1].find(sep):
+            print "Found " + sep + " in line " + desc_line
             desc_split = desc_line[1].split(sep)
             transaction_dict['customer'] = desc_split[0]
             transaction_dict['description'] = desc_split[1]
