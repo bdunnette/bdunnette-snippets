94b902f90d9635c523c0ebc93adf256305fe3752
Brian Dunnette <dunn0172@umn.edu>
2012-01-11 13:36:08 -0600

    / ‘ledger-invoice.py’

--- ledger-invoice.py
+++ ledger-invoice.py
@@ -2,9 +2,14 @@
 
 from relatorio.templates.opendocument import Template
 from data import bonham_invoice
+import sys
+
+infile = file(sys.argv[1]).read()
+transactions = infile.split('\n\n')
+print transactions
 
 basic = Template(source=None, filepath='invoice.odt')
 basic_generated = basic.generate(o=bonham_invoice).render()
-file('bonham_invoice.odt', 'wb').write(basic_generated.getvalue())
+#file('bonham_invoice.odt', 'wb').write(basic_generated.getvalue())
 
 
