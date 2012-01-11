508feae7de7e460aa90cadf030d129a547cedd7a
Brian Dunnette <dunn0172@umn.edu>
2012-01-11 16:49:47 -0600

    / ‘ledger-invoice.py’

--- ledger-invoice.py
+++ ledger-invoice.py
@@ -37,7 +37,7 @@ for transaction in transactions:
             
     print transaction_dict
     invoice_generated = invoice_template.generate(invoice=transaction_dict).render()
-    outfile = transaction_dict['date'].replace('/', '-') + transaction_dict['customer'] + '.odt'
+    outfile = transaction_dict['date'].replace('/', '-') + transaction_dict['customer'].replace(' ', '_') + '.odt'
     outfile = outfile.strip()
     file(outfile, 'wb').write(invoice_generated.getvalue())
 
