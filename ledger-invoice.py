1bae7732646d19850da4807231ca9b987f5f1ca6
Brian Dunnette <dunn0172@umn.edu>
2012-01-11 13:40:23 -0600

    / ‘ledger-invoice.py’

--- ledger-invoice.py
+++ ledger-invoice.py
@@ -8,7 +8,7 @@ infile = file(sys.argv[1]).read()
 transactions = infile.split('\n\n')
 
 for line in transactions:
-    print line.split('\n')[1]
+    print line.split('\n')[0]
 
 basic = Template(source=None, filepath='invoice.odt')
 basic_generated = basic.generate(o=bonham_invoice).render()
