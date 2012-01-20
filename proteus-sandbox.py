9d2450118ebded6daca57284e0272d6e30025633
Brian Dunnette <dunn0172@umn.edu>
2012-01-20 14:29:14 -0600

    / ‘proteus-sandbox.py’

--- proteus-sandbox.py
+++ proteus-sandbox.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 from proteus import config, Model, Wizard
 
-config = config.set_xmlrpc('http://admin:test@localhost:8069')
-Party = Model.get('party.party')
-party = Party.find([('name', '=', 'Buster Bot')])
-print party
+config = config.set_xmlrpc('http://admin:test@localhost:8069/test')
+Sale = Model.get('sale.sale')
+sales = Sale.find()
+print sales
