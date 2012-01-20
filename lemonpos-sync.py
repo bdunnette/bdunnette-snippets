8db7b799a00e7cdb7d0ae7aa53af3de31ef550b9
Brian Dunnette <dunn0172@umn.edu>
2012-01-20 11:28:06 -0600

    / ‘lemonpos-sync.py’

--- lemonpos-sync.py
+++ lemonpos-sync.py
@@ -1,4 +1,4 @@
 from storm.locals import *
 
-database = create_database("mysql://root:test@localhost:3306/lemonpos")
+database = create_database("mysql://root:test@localhost:3306/lemondb")
 store = Store(database)
