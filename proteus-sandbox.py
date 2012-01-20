bf98b52fa01d0c4243e5db8db02dbefa6ca216d5
Brian Dunnette <dunn0172@umn.edu>
2012-01-20 14:31:04 -0600

    / ‘proteus-sandbox.py’

--- proteus-sandbox.py
+++ proteus-sandbox.py
@@ -4,4 +4,5 @@ from proteus import config, Model, Wizard
 config = config.set_xmlrpc('http://admin:test@localhost:8069/test')
 Sale = Model.get('sale.sale')
 sales = Sale.find()
-print sales
+for s in sales:
+    print "%s: %s - %s" % (s.id, s.party, s.description)
