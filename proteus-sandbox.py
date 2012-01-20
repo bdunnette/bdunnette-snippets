b63e05c139cd90f1ecd33501f29170d940fbaa08
Brian Dunnette <dunn0172@umn.edu>
2012-01-20 14:32:38 -0600

    / ‘proteus-sandbox.py’

--- proteus-sandbox.py
+++ proteus-sandbox.py
@@ -5,4 +5,4 @@ config = config.set_xmlrpc('http://admin:test@localhost:8069/test')
 Sale = Model.get('sale.sale')
 sales = Sale.find()
 for s in sales:
-    print "%s: %s - %s" % (s.id, s.party.name, s.description)
+    print s.id, s.party.name, s.description, s.state, s.invoice_state, s.write_date, s.create_date
