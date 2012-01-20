9391641753a497396a182d4cd0372a2014908a7d
Brian Dunnette <dunn0172@umn.edu>
2012-01-20 14:31:16 -0600

    / ‘proteus-sandbox.py’

--- proteus-sandbox.py
+++ proteus-sandbox.py
@@ -5,4 +5,4 @@ config = config.set_xmlrpc('http://admin:test@localhost:8069/test')
 Sale = Model.get('sale.sale')
 sales = Sale.find()
 for s in sales:
-    print "%s: %s - %s" % (s.id, s.party, s.description)
+    print "%s: %s - %s" % (s.id, s.party.name, s.description)
