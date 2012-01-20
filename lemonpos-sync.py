4756a44b933b630c70e23165443e893f77be94bf
Brian Dunnette <dunn0172@umn.edu>
2012-01-20 13:24:44 -0600

    / ‘lemonpos-sync.py’

--- lemonpos-sync.py
+++ lemonpos-sync.py
@@ -12,8 +12,8 @@ try:
     lcon = lemondb.connect(host="localhost", user="root", passwd="test", db="lemondb")
     config = config.set_xmlrpc("http://admin:test@localhost:8069/test")
     Party = Model.get('party.party')
-    party = Party.find([('name', '=', 'Buster Bot')])
-    print party
+    #party = Party.find([('name', '=', 'Buster Bot')])
+    print Party
     cur = lcon.cursor(lemondb.cursors.DictCursor)
     cur.execute("SELECT * FROM products")
     data = cur.fetchall()
