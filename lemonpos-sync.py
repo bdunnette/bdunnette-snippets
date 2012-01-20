3f5940a1ae2b70d9fd5c52650921c3bca8c5c6e0
Brian Dunnette <dunn0172@umn.edu>
2012-01-20 11:29:08 -0600

    / ‘lemonpos-sync.py’

--- lemonpos-sync.py
+++ lemonpos-sync.py
@@ -1,4 +1,9 @@
 from storm.locals import *
 
+class Person(object):
+    __storm_table__ = "person"
+    id = Int(primary=True)
+    name = Unicode()
+
 database = create_database("mysql://root:test@localhost:3306/lemondb")
 store = Store(database)
