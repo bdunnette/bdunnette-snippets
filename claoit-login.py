308b700f569d8d5e1ae0295fdb76ffb2c1da55e7
Brian Dunnette <dunn0172@umn.edu>
2012-03-08 08:10:53 -0600

    / ‘claoit-login.py’

--- claoit-login.py
+++ claoit-login.py
@@ -5,10 +5,11 @@ import urllib2
 
 base_url = "http://legacy.claoit.umn.edu/inout/claoit/index.php?controller=edit&action=set_in&userid="
 
+# Get the current user, using the getpass module
 current_user = getpass.getuser()
 
+# Append the current username to the end of our base URL
 full_url = base_url + current_user
-print full_url
 
+# Fetch data from the full URL - in our case, simply doing a "GET" sets that user as "signed in"
 signed_in = urllib2.urlopen(full_url)
-print signed_in
