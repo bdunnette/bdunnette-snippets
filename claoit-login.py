0a4edc40c6a3f8f9145d8eae65d825d3d7ed144c
Brian Dunnette <dunn0172@umn.edu>
2012-03-08 08:08:33 -0600

    / ‘claoit-login.py’

--- claoit-login.py
+++ claoit-login.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python
 
 import getpass
+import urllib2
 
 base_url = "http://legacy.claoit.umn.edu/inout/claoit/index.php?controller=edit&action=set_in&userid="
 
 current_user = getpass.getuser()
-print base_url + current_user
+
+full_url = base_url + current_user
+print full_url
+
+signed_in = urllib2.urlopen(full_url)
+print signed_in
